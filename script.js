dfb855667b06345338b0b8b4a85d76f585eed821
10cbr <93221409+10cbr@users.noreply.github.com>
2021-10-27 00:42:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,7 @@ function getHistory(){
 	return document.getElementById("history-value").innerText;
 }
 function printHistory(num){
-	document.getElementById("history-value").innerText=num;
-}
-function getOutput(){
-	return document.getElementById("output-value").innerText;
-}
-function printOutput(num){
-	if(num==""){
-		document.getElementById("output-value").innerText=num;
-	}
-	else{
-		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	.innerText=getFormattedNumber(num);
 	}	
 }
 function getFormattedNumber(num){
