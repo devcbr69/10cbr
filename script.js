e337aa422a9c83a1ffa5bbec037b513fd069ba96
bhagyaqw <92821708+bhagyaqw@users.noreply.github.com>
2022-10-31 23:34:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,13 @@ for(var i =0;i<operator.length;i++){
 				output= output.substr(0,output.length-1);
 				printOutput(output);
 			}
+			width
+	background-color: #eaedef;
+	margin: 0 auto;
+	top: 20px;
+	position: relative;
+	border-radius: 5px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 		}
 		else{
 			var output=getOutput();
